70e5e9afdb92e27e8a0dc767f96dd9401f065acf
Mohammed Anas <anasmd021@gmail.com>
2026-02-03 16:35:06 +0530

    jwt fixed for 15 mins expiry

--- config/admin.js
+++ config/admin.js
@@ -1,20 +1,21 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
+    expiresIn: 900, // 15 minutes in seconds
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env("ENCRYPTION_KEY"),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
