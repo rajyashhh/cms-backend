3696efd5257e87af9ccc08b9b10eddb22d2f486b
Mohammed Anas <anasmd021@gmail.com>
2026-02-17 00:38:33 +0530

    fix

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -28,18 +28,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       ...pagination,
     });
 
-    // Add itemsSummary and remove items from each order
-    const ordersWithSummary = orders.map((order) => {
-      const items = order.items || [];
-      order.itemsSummary = items
-        .map((item) => `${item.productName} (x${item.quantity})`)
-        .join(", ");
-      delete order.items;
-      return order;
-    });
-
     // Return in REST API format
-    return { data: ordersWithSummary };
+    return { data: orders };
   },
 
   async findOne(ctx) {
@@ -55,14 +45,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       return ctx.unauthorized("You cannot access this order");
     }
 
-    // Add itemsSummary and remove items
-    const items = entity.items || [];
-    entity.itemsSummary = items
-      .map((item) => `${item.productName} (x${item.quantity})`)
-      .join(", ");
-    delete entity.items;
-
-    return { data: entity };
+    return await super.findOne(ctx);
   },
 
   async create(ctx) {
